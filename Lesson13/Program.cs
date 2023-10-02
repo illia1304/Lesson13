f6098987b6e8059397685e5d94ba60bcdde5217e
Illia <1304illia@gmail.com>
2023-10-02 12:41:13 +0300

    factory

--- Lesson13/Program.cs
+++ Lesson13/Program.cs
@@ -65,18 +65,80 @@
                 Console.WriteLine("additional functionality");
             }
         }
-        
+        //Abstract Factory
+
+        public abstract class Toy
+        {
+            public abstract void Play();
+        }
+
+        public class Doll : Toy
+        {
+            public override void Play()
+            {
+                Console.WriteLine("Playing with doll");
+            }
+        }
+        public class Car : Toy
+        {
+            public override void Play()
+            {
+                Console.WriteLine("Playing with car");
+            }
+        }
+        public class SoftToy : Toy
+        {
+            public override void Play()
+            {
+                Console.WriteLine("Playing with soft toy");
+            }
+        }
+
+        public abstract class ToyFactory
+        {
+            public abstract Toy CreateDoll();
+            public abstract Toy CreateCar();
+            public abstract Toy CreateSoftToy();
+        }
+
+        public class ConcreteToyFactory : ToyFactory
+        {
+            public override Toy CreateDoll()
+            {
+                return new Doll();
+            }
+            public override Toy CreateCar()
+            {
+                return new Car();
+            }
+            public override Toy CreateSoftToy()
+            {
+                return new SoftToy();
+            }
+        }
         public static void Main(string[] args)
         {
+            //singleton
             SingleTon s1 = SingleTon.Initialize();
             SingleTon s2 = SingleTon.Initialize();
 
             Console.WriteLine(s1 == s2 ? 1 : 0);
 
+            //decorator
             IComponent component = new ConcreteComponent();
             component = new ConcreteDecorator(component);
 
             component.Operation();
+
+            //factory
+            ToyFactory factory = new ConcreteToyFactory();
+            Toy doll = factory.CreateDoll();
+            Toy car = factory.CreateCar();
+            Toy softtoy = factory.CreateSoftToy();
+
+            doll.Play();
+            car.Play(); 
+            softtoy.Play();
         }
     }
 }
