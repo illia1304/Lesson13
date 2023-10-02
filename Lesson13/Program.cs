c1f459545ca757c0059cdf9e3006b999d5cade54
Illia <1304illia@gmail.com>
2023-10-02 10:02:01 +0300

    Add project files.

---
+++ Lesson13/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Lesson13
+{
+    public partial class Program
+    {
+        class SingleTon
+        {
+            private static SingleTon single = null;
+
+            protected SingleTon()
+            {
+
+            }
+
+            public static SingleTon Initialize()
+            {
+                if (single == null)
+                {
+                    single = new SingleTon();
+                }
+
+                return single;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            SingleTon s1 = SingleTon.Initialize();
+            SingleTon s2 = SingleTon.Initialize();
+
+            Console.WriteLine(s1 == s2 ? 1 : 0);
+        }
+    }
+}
